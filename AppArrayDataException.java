e156778361eb2dd93bc324f2be488a6eabb3ed48
Ruslan <suhorukovrus0@gmail.com>
2024-10-25 20:36:06 +0500

    final

--- AppArrayDataException.java
+++ AppArrayDataException.java
@@ -1,7 +1,5 @@
 package ru.kaznu.ruslan.exceptions;
 
 public class AppArrayDataException extends RuntimeException { //создание своего исключения AppArraySizeException, на основе RuntimeException
-    public AppArrayDataException(String message) {
-        super(message);
-    }
+
 }
--- AppArraySizeException.java
+++ AppArraySizeException.java
@@ -1,7 +1,5 @@
 package ru.kaznu.ruslan.exceptions;
 
 public class AppArraySizeException extends RuntimeException { //создание своего исключения AppArraySizeException, на основе RuntimeException
-    public AppArraySizeException(String message) {
-        super(message);
-    }
+
 }
